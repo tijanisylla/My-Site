f3a96e4ba6078352451dbaa2d96586f99e035aa1
tijani sylla <tijanisylla@tijanis-Air.attlocal.net>
2021-08-04 02:01:17 -0500

    new commit

--- server/server.js
+++ server/server.js
@@ -75,14 +75,19 @@ app.post("/send", function (req, res) {
 
 
 //======Deploying settings=====
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('build'))
-  app.get('*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-  })
-}
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static('build'))
+//   app.get('*', (req, res) => {
+//     req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+//   })
+// }
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 //Step 1
